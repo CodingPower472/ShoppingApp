a19513fe256a2ba8f1e80360fc30c3f0ce92b067
Luke <tech.master.champ@gmail.com>
2015-08-02 14:36:14 +0000

    Added JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('#add').click(function () {
+    $('#list').append('<li>' + $('#item').val() + '</li>');
+    $('#item').val('');
+  });
+  $(document).keyup(function () {
+    if (event.which === 13) {
+      $('#add').trigger('click');
+    }
+  });
+});
